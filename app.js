38a9033ddd1d211d45bac3aaebfc576ee6580b02
henry walton <hnrywltn@gmail.com>
2021-06-29 14:08:29 -0700

    pugs/validations/routing

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products');
 
 const app = express();
 
@@ -39,6 +40,7 @@ store.sync();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/products.js
+++ routes/products.js
@@ -1,1 +1,96 @@
 const express = require('express');
+const router = express.Router();
+
+const { User, Product } = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils');
+const { check, validationResult } = require('express-validator');
+
+
+
+
+///////////////TODO!! ----  grab the userId
+router.get('/submit', csrfProtection, function(req, res, next) {
+  console.log(req.session)
+  // const userId = req.session.auth.user
+  res.render('submit-product', {
+    title: 'Submit Extension',
+    // userId,
+    csrfToken: req.csrfToken(),
+  });
+});
+////////////////////////////////////////////
+
+const productValidators = [
+  check('productName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Extension Name')
+    .isLength({max: 50})
+    .withMessage('Too long fool'),
+  check('imgUrl')
+    .isLength({max: 400})
+    .withMessage('Too long fool'),
+  check('productUrl')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Extension URL')
+    .isLength({max: 400})
+    .withMessage('Too long fool'),
+  check('categoryId')
+    .exists({ checkFalsy: true })
+    .withMessage('Select a category fool'),
+  check('description')
+    .exists({ checkFalsy: true })
+    .withMessage('Tell us about your extension fool'),
+  check('userId')
+    .exists({ checkFalsy: true })
+    .withMessage('Stop hacking around fool')
+];
+
+router.post('/submit', csrfProtection, productValidators, asyncHandler(async(req, res, next) => {
+  const {
+    productName,
+    imgUrl: imgUrl,
+    productUrl,
+    categoryId,
+    description,
+    userId
+  } = req.body;
+
+
+  let errors = [];
+
+  const validatorErrors = validationResult(req);
+
+  if(validatorErrors.isEmpty()) {
+    const extension = Product.create({
+      productName,
+      imgUrl,
+      productUrl,
+      categoryId,
+      userId,
+      description
+    });
+
+    return res.redirect('/');
+  } else {
+    errors = validatorErrors.array().map((error) => error.msg);
+  }
+
+  res.render('submit-product', {
+    title: 'Submit Extension',
+    productName,
+    imgUrl,
+    productUrl,
+    categoryId,
+    description,
+    userId,
+    errors,
+    csrfToken: req.csrfToken(),
+  });
+}));
+
+
+
+
+
+
+module.exports = router;
